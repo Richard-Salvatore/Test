98375aa980ccec8f26d31ef9423806ea7751d2f2
Richard-Salvatore <166079004+Richard-Salvatore@users.noreply.github.com>
2024-10-29 22:27:11 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -187,9 +187,8 @@ local function CartBringFunction(targetPlayerName)
     if controller and controller.Character then
         local controllerPrimaryCFrame = controller.Character.HumanoidRootPart.CFrame
         localPlayer.Character.HumanoidRootPart.CFrame = controllerPrimaryCFrame
-        -- Clear existing tools
+        wait(2)
         game:GetService("ReplicatedStorage").RE["1Clea1rTool1s"]:FireServer("ClearAllTools")
-        wait(1)
         plr.Humanoid.Sit = false
     end
 end
